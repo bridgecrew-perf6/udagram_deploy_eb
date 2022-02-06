9e922eb0771481a836e13097e4ee927430112313
Mostafa Said <mostafa.said.ext@orange.com>
2022-02-06 13:25:21 +0200

    ci_cd

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,3 @@
-eb init udagram-Project --platform node.js --region us-east-1
+cd udagram-api && eb init udagram-Project -r us-east-1 -p node.js-12
+cd udagram-api && eb deploy Udagramproject-env
+cd udagram-api && eb status
