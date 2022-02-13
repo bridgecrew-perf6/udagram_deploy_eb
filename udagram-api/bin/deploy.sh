4e941149ac05e7ec42cfb5970539e2e45d431a5c
Mostafa Said <mostafa.said.ext@orange.com>
2022-02-13 23:52:18 +0200

    fix

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,4 @@
 eb init udagram-Project -r us-east-1 -p node.js-12
+eb setenv AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION  JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DATABASE=$POSTGRES_DATABASE POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME aws_access_key_id=$aws_access_key_id aws_secret_access_key=$aws_secret_access_key
 eb deploy Udagramproject-env
 eb status
